948d437f3bcf99c55e880d134bb41d3ccc32ff4e
Spawn9986 <scouch9986@gmail.com>
2023-03-29 21:02:54 -0500

    added an array of animals and a getRandomAnimal function to generate new animal each time the user clicks the button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
-    return <div>Show animal list here!</div>
+    const [animals, setAnimals] = useState([]);
+
+    //Add event handler that will run reference the entire handleClick function and call/invoke the function "in the future" anytime the user clicks the button below
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+    
+    return <div>
+        {/*pass the handleClick function as a prop to button*/}
+        <button onClick={handleClick}>Add Animal</button>
+        <div>{animals}</div>
+        
+    </div>
 }
 
 export default App;
